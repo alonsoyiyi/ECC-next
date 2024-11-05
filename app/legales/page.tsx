b72477411172da5a3f5fdff9099b8d32362faf10
angelalonsopazruiz <angel.paz.consultas@gmail.com>
2024-11-04 22:04:33 -0500

    depurando errores de deploy 4

--- app/legales/page.tsx
+++ app/legales/page.tsx
@@ -1,12 +1,19 @@
-// app/legales/page.tsx
-'use client';
+'use client'
 
-import LegalDetail from '@/components/LegalDetail';
+import LegalDetail from '@/components/LegalDetail'
+import { GlobalInputs } from '@/types/globalTypes'
 
-export default function LegalPage() {
+export default function LegalesPage() {
+  const emptyGlobalInputs: GlobalInputs = {
+    nombre: '',
+  }
+
   return (
     <div className="container mx-auto p-4">
-      <LegalDetail selectedLegalId={'1'} globalInputs={{}} /> {/* Pasamos directamente el ID del documento */}
+      <LegalDetail 
+        selectedLegalId="1" 
+        globalInputs={emptyGlobalInputs}
+      />
     </div>
-  );
+  )
 }
