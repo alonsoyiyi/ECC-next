eb23d3a912fe0cd4b2707f9151c48aace31e7d82
angelalonsopazruiz <angel.paz.consultas@gmail.com>
2024-11-04 20:39:53 -0500

    depurando errores de deploy

--- components/SalesList.tsx
+++ components/SalesList.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 import SalesDetail from './SalesDetail';
 import { Button } from "@/components/ui/button"; // Asegúrate de que Button esté importado
 
