c4c6afd248f841b478602a28d98567b419c44efb
angelalonsopazruiz <angel.paz.consultas@gmail.com>
2024-11-26 11:40:14 -0500

    se corrigen types

--- pages/api/getData.ts
+++ pages/api/getData.ts
@@ -1,28 +1,35 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import fs from 'fs'
+import fs from 'fs/promises'
 import path from 'path'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+interface GeneralDataItem {
+  id: string
+  label: string
+  message: string
+  data?: Record<string, unknown>
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { category } = req.query
 
   if (!category || typeof category !== 'string') {
     return res.status(400).json({ error: 'Category parameter is required' })
   }
 
-  // Definir la ruta al archivo JSON
   const filePath = path.join(process.cwd(), 'server', `${category}.json`)
 
-  // Verificar que el archivo exista antes de leerlo
-  if (!fs.existsSync(filePath)) {
-    return res.status(404).json({ error: 'File not found' })
-  }
-
   try {
-    const fileContents = fs.readFileSync(filePath, 'utf8')
-    const data = JSON.parse(fileContents)
+    const fileContents = await fs.readFile(filePath, 'utf8')
+    const data: Record<string, GeneralDataItem> = JSON.parse(fileContents)
     res.status(200).json(data)
-  } catch (error) {
-    res.status(500).json({ error: 'Error reading file' })
-    console.error(error)
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      // Ahora 'error' es un objeto de tipo Error
+      console.error(error.message)
+      res.status(500).json({ error: 'Error reading file' })
+    } else {
+      // Si el error no es una instancia de Error, manejamos ese caso
+      res.status(500).json({ error: 'Unknown error occurred' })
+    }
   }
 }
